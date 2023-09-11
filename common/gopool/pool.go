e12de3cbd2f9a46889da3cd99741d29011078713
0xcb9ff9 <0xcb9ff9@proton.me>
2023-09-11 14:47:45 +0800

    common/gopool: upgrade ants package version v2.8.0

--- common/gopool/pool.go
+++ common/gopool/pool.go
@@ -2,14 +2,13 @@ package gopool
 
 import (
 	"runtime"
-	"time"
 
 	"github.com/panjf2000/ants/v2"
 )
 
 var (
 	// Init a instance pool when importing ants.
-	defaultPool, _   = ants.NewPool(ants.DefaultAntsPoolSize, ants.WithExpiryDuration(time.Hour))
+	defaultPool, _   = ants.NewPool(ants.DefaultAntsPoolSize, ants.WithDisablePurge(true), ants.WithPreAlloc(true))
 	minNumberPerTask = 5
 )
 
